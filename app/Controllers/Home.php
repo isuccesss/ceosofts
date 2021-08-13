54aea136f426f41bea7648d41913b94c1efa386d
isuccesss <isuccess@outlook.co.th>
2021-08-13 09:53:58 +0700

    revise

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message'); //ให้ตรงกับชื่อ file ใน views
+		return view('welcome_message'); //ให้ตรงกับชื่อ file ใน views ok
 	}
 }
